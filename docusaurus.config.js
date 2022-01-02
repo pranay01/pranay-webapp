92cd85252c7502637a0f5383bffc5c05546f6dd5
Pranay Prateek <pranay@signoz.io>
2022-01-02 15:25:22 +0530

    updating title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Pranay\'s Site',
-  tagline: 'Tutorial for Priyanka',
+  title: 'But who will monitor the monitor?',
+  tagline: 'Essays and Meditations',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
